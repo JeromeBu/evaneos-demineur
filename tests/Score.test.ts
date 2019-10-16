3b47acf42438041d9d5416860bfa54449afef7d4
Jerome Burkard <jerome.burkard@gadz.org>
2019-10-16 21:39:44 +0200

    add test to check score when time passes

--- tests/Score.test.ts
+++ tests/Score.test.ts
@@ -1,5 +1,5 @@
 import { Cell } from '../src/Domain/Cell';
-import { Score } from '../src/Domain/Score';
+import { Score, Trigger } from '../src/Domain/Score';
 import { Grid } from '../src/Domain/Grid';
 
 describe('counts score', () => {
@@ -9,7 +9,8 @@ describe('counts score', () => {
         const columns = 3;
         const cells: Cell[] = [cellWithBomb, ...Array(8).fill(cellWithoutBomb)];
         const initialScore = cells.length;
-        const score = new Score(initialScore);
+        const triggersNever: Trigger = () => {};
+        const score = new Score(initialScore, triggersNever);
 
         const initialGrid = new Grid(columns, cells, score);
 
@@ -20,5 +21,23 @@ describe('counts score', () => {
 
         const canceledGrid = dugGrid.cancelLastShot()!;
         expect(canceledGrid.score).toEqual(initialScore - 1 - 5);
+    });
+    test('handles time passing', () => {
+        const cellWithBomb = Cell.withBomb();
+        const cellWithoutBomb = Cell.withoutBomb();
+        const columns = 3;
+        const cells: Cell[] = [cellWithBomb, ...Array(8).fill(cellWithoutBomb)];
+        const initialScore = cells.length;
+        const triggersInstantly: Trigger = callback => {
+            callback();
+        };
+        const score = new Score(initialScore, triggersInstantly);
+
+        const initialGrid = new Grid(columns, cells, score);
+
+        const substract = (intialValue: number, toRemove: number) =>
+            Math.round(Math.max(0, intialValue - toRemove) * 10) / 10;
+
+        expect(initialGrid.score).toEqual(substract(initialScore, 0.2));
     });
 });
